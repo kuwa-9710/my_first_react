a142ae6715850fe92af0bcb697b55a4c4ff4198a
tomoya <shima.apple0941@gmail.com>
2022-12-14 23:42:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  // let items = [
+  //   { name: "stroberry", price: "100" },
+  //   { name: "apple", price: "150" },
+  //   { name: "banana", price: "230" },
+  // ];
+
+  // return (
+  //   <div className="App container mt-5">
+  //     <table className="table table-striped">
+  //       <tbody>
+  //         {items.map((value) => (
+  //           <tr>
+  //             <th scope="row">{value.name}</th>
+  //             <td>{value.price}yen</td>
+  //           </tr>
+  //         ))}
+  //       </tbody>
+  //     </table>
+  //   </div>
+  // );
+
+  return(
+    <div className="container text-center mt-5">
+      <Clock />
     </div>
-  );
+  )
+}
+
+// componentの定義
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.now = new Date();
+    this.time = ``;
+
+    this.state = {
+      time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+    }
+
+    this.refresh = this.refresh.bind(this);
+  }
+
+  refresh() {
+    this.setState((state) => ({
+      time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+    }));
+  }
+
+  render() {
+    return <p onClick={this.refresh}>{this.state.time}</p>
+  }  
 }
 
 export default App;
